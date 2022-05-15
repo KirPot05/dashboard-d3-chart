d7aaddfc5775f5c01262267373cdaeaa08bdaa25
Kiran <kiran0503@gmail.com>
2022-05-15 13:25:09 +0530

    Firebase initialization

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvg_pajHapdh0Jk-dOtAch_Ky4xj3-ekM",
+  authDomain: "d3-dashboard-2153d.firebaseapp.com",
+  projectId: "d3-dashboard-2153d",
+  storageBucket: "d3-dashboard-2153d.appspot.com",
+  messagingSenderId: "687674747298",
+  appId: "1:687674747298:web:3fb66d2ccb9c6e8435aafb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
