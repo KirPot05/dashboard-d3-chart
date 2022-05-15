eef28b04fa21132cf08a5d076ecb7b7012e46df2
Kiran <kiran0503@gmail.com>
2022-05-15 13:12:48 +0530

    Fixed client side routing

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename={location.origin}>
+		<BrowserRouter basename={window.location.origin}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
