c8ee20331e62336d1982c799935abd70213db655
Kiran <kiran0503@gmail.com>
2022-05-15 13:37:10 +0530

    Deployed to firebase

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Home from './components/Home';
 function App() {
 	return (
 		<Routes>
+			<Route exact path='/' element={<Home/>}/>
 			<Route exact path='/register' element={<Dashboard/>}/>
-			<Route path='/' element={<Home/>}/>
 		</Routes>
 	);
 }
